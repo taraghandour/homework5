19d6420424102d1e143629e1b348626bd8465606
tara ghandour <tg2750@columbia.edu>
2022-03-23 04:07:30 -0400

    final edits

--- homework5/R/function 2.R
+++ homework5/R/function 2.R
@@ -1,6 +1,6 @@
 #' Oldest Player function
 #'
-#' This function allows you to
+#' This function allows you to know the oldest player on the NBA for any given year
 #' @param year the year for which you want to know the oldest player
 #' @keywords Oldest player
 #' @export
